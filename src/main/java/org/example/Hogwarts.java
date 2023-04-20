f5489e04948b16537f96056fd8a6dc2b2341787b
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-04-20 16:18:46 +0300

    Теперь наследует метод

--- src/main/java/org/example/Hogwarts.java
+++ src/main/java/org/example/Hogwarts.java
@@ -4,13 +4,22 @@ public abstract class Hogwarts {
     private String firstName;
     private String secondName;
     private int powerMagic;
-    protected int transgression;
+    private int transgression;
 
     public Hogwarts(String firstName, String secondName, int powerMagic, int transgression) {
         this.firstName = firstName;
         this.secondName = secondName;
         this.powerMagic = powerMagic;
         this.transgression = transgression;
+    }
+    public void betterStudentInHogwarts(Hogwarts person2) {
+        if (transgression > person2.getTransgression()) {
+            System.out.println(getFirstName() + " Лучшее, чем " + person2.getFirstName());
+        } else if (transgression < person2.getTransgression()) {
+            System.out.println(person2.getFirstName() + " Лучшее, чем " + getFirstName());
+        } else {
+            System.out.println(getFirstName() + " и " + person2.getFirstName() + " равны");
+        }
     }
 
     public int getPowerMagic() {
--- src/main/java/org/example/Slytherin.java
+++ src/main/java/org/example/Slytherin.java
@@ -33,15 +33,6 @@ public class Slytherin extends Hogwarts {
         }
     }
 
-    public void betterStudentInHogwarts(Hogwarts person2) {
-        if (transgression > person2.getTransgression()) {
-            System.out.println(getFirstName() + " Лучшее, чем " + person2.getFirstName());
-        } else if (transgression < person2.getTransgression()) {
-            System.out.println(person2.getFirstName() + " Лучшее, чем " + getFirstName());
-        } else {
-            System.out.println(getFirstName() + " и " + person2.getFirstName() + " равны");
-        }
-    }
 
     @Override
     public String toString() {
